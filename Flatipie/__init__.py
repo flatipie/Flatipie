aa1348d479ea0e113e0f29796906c43e5295193e
@flatipieqt <74166653+flatipie@users.noreply.github.com>
2020-11-14 15:34:16 +0800

    Update __init__.py

--- Flatipie/__init__.py
+++ Flatipie/__init__.py
@@ -2,8 +2,8 @@
 #from .widgets import *
 from .build import apply_palette, ModernWindow
 
-__title__ = "Flatify"
-__version__ = "0.1"
+__title__ = "Flatipie"
+__version__ = "0.2"
 __copyright__ = "Copyright (c) 2020, Zenqi. All rights reserved."
 __liscense__ = "MIT"
 __author__ = "Zenqi"
